b739ccea10ab299536310dd792c0cdbdc0b8cdb8
Nicholas Zhang <nicholasz2510@gmail.com>
2020-04-29 01:35:41 -0600

    Update meeting_bot.py

--- meeting_bot.py
+++ meeting_bot.py
@@ -1,30 +1,37 @@
 import discord
-from requests import Session
-from bs4 import BeautifulSoup as bs
+import html2text
+from canvasapi import Canvas
+from canvasapi.page import Page
 
 f = open("config.txt", "r")
 config_info = f.readlines()
 f.close()
 
+# Canvas API URL
+API_URL = "https://graniteschools.instructure.com/api/v1"
+# Canvas API key
+API_KEY = config_info[3]
 
-def scrape():
-    with Session() as s:
-        site = s.get("https://graniteschools.instructure.com/login/ldap")
-        bs_content = bs(site.content, "html.parser")
-        token = bs_content.find("input", {"name": "authenticity_token"})["value"]
-        login_data = {"pseudonym_session[unique_id]": config_info[1], "pseudonym_session[password]": config_info[2],
-                      "authenticity_token": token}
-        s.post("https://graniteschools.instructure.com/login/ldap", login_data)
-        home_page = s.get("https://graniteschools.instructure.com/courses/1337964/modules")
 
-        soup = bs(home_page.text, 'html.parser')
-        weeks = [x for x in soup.find_all("a", class_='ig-title title item_link') if x["title"].startswith("Week of ")]
-        curr_week = weeks[len(weeks) - 1]
-        # print(curr_week)
+def get_info():
+    canvas = Canvas(API_URL, API_KEY)
 
-        curr_page = s.get("https://graniteschools.instructure.com" + curr_week["href"])
-        # print(curr_page.text)
-        return "https://graniteschools.instructure.com" + curr_week["href"]
+    course = canvas.get_course(1337964)
+    pages = [str(html2text.html2text(Page.show_latest_revision(p).body)) for p in course.get_pages() if p.title.startswith("Week of")]
+    page_lines = pages[len(pages) - 1].splitlines()
+    meeting_i_start = -1
+    meeting_i_end = -1
+    for i in range(len(page_lines)):
+        if "Lecture/Discussion" in page_lines[i]:
+            meeting_i_start = i
+            break
+    for j in range(len(page_lines) - 1, 0, -1):
+        if "Password: " in page_lines[j]:
+            meeting_i_end = j + 1
+            break
+    if meeting_i_end == -1 or meeting_i_start == -1:
+        return "I couldn't find the meeting information for the current week."
+    return "\n".join(page_lines[meeting_i_start:meeting_i_end])
 
 
 class MyClient(discord.Client):
@@ -41,7 +48,7 @@ class MyClient(discord.Client):
 
         elif message.content.startswith('-nextmeeting'):
             await message.channel.send('Let me check...')
-            await message.channel.send('The information is on this page:\n' + scrape())
+            await message.channel.send(get_info()[:1999])
 
         elif message.content.startswith('-'):
             await message.channel.send('Hello! I\'m a Discord bot <@449579826278563860> made!\nMy prefix is `-`\nFor '
